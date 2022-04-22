8719beb8a336a06e2407572fb947ace2c001412e
kupessa <loris.kupessa@gmail.com>
2022-04-22 22:13:06 +0100

    Details page

--- components/index.js
+++ components/index.js
@@ -2,5 +2,8 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NTFCard from "./NTFCard";
 import { CircleButton, RectButton } from "./Button";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import {SubInfo} from './SubInfo';
 
-export {HomeHeader, FocusedStatusBar, NTFCard, CircleButton, RectButton}
+export {HomeHeader, FocusedStatusBar, NTFCard, CircleButton, RectButton, DetailsBid, DetailsDesc, SubInfo}
